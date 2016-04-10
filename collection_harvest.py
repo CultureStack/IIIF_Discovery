f4578792a53df5c6aa46dab4ab022ca10852043a
Matthew McGrattan <matthew.mcgrattan@bodleian.ox.ac.uk>
2016-04-10 18:03:29 +0100

    Changed the import to import iiif_collections rather than from iiif_collections import, and PEp8 formatting changes.

--- collection_harvest.py
+++ collection_harvest.py
@@ -6,17 +6,32 @@ within those collections, and compiling a list of manifests.
 Dumps the list of manifests to a json file.
 
 '''
-from iiif_collections import IIIF_Collection, IIIF_Item
+import iiif_collections
 import json
 
 top_list = []
+master_list = []
 
 #top_level_collections = json.loads(IIIF_Item('https://raw.githubusercontent.com/ryanfb/iiif-universe/gh-pages/iiif-universe.json').source_data)
 collection_json = open('iiif-universe-small.json').read()
 top_level_collections = json.loads(collection_json)
 for top_level_collection in top_level_collections['collections']:
     print 'Working on: ' + top_level_collection['label']
-    top_list.append({top_level_collection['@id']: top_level_collection['label'],
-                     'manifests': IIIF_Collection(top_level_collection['@id']).master_manifest_list})
+    top_list.append(
+        {'@context':
+         'http://iiif.io/api/presentation/2/context.json',
+         '@id': top_level_collection['@id'],
+         '@type': 'sc:Collection',
+         'label': top_level_collection['label'],
+         'manifests': iiif_collections.IIIF_Collection(
+             top_level_collection['@id']).master_manifest_list}
+    )
 
-json.dump(top_list, open('master.json', 'wb'), sort_keys=True, indent=4)
+master_list.append(
+    {'@context':
+     'http://iiif.io/api/presentation/2/context.json',
+     '@id': 'http://blahblah/all.json',
+     'label': 'Small Universe Harvest',
+     '@type': 'sc:Collection',
+     'collections': top_list})
+json.dump(master_list, open('master.json', 'wb'), sort_keys=True, indent=4)
